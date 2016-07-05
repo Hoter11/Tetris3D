84227fbf0b61c4d408da0561cf69f488cfb03859
roc <granadaroc96@gmail.com>
2016-07-05 02:28:35 +0200

    Main activity

--- app/src/main/java/company/roc/tetris3d/MainActivity.java
+++ app/src/main/java/company/roc/tetris3d/MainActivity.java
@@ -1,7 +1,12 @@
 package company.roc.tetris3d;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnPlay = (Button) findViewById(R.id.btnPlay);
+        Button btnRank = (Button) findViewById(R.id.btnRank);
+        Button btnConf = (Button) findViewById(R.id.btnConf);
+        Button btnShop = (Button) findViewById(R.id.btnShop);
+
+        buttonEffect(btnPlay);
+        buttonEffect(btnRank);
+        buttonEffect(btnConf);
+        buttonEffect(btnShop);
+
+    }
+
+
+
+    //BUTTON EFFECT
+    public static void buttonEffect(View button){
+        button.setOnTouchListener(new View.OnTouchListener() {
+
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN: {
+                        v.getBackground().setColorFilter(0x80ffffff, PorterDuff.Mode.SRC_ATOP);
+                        v.invalidate();
+                        break;
+                    }
+                    case MotionEvent.ACTION_UP: {
+                        v.getBackground().clearColorFilter();
+                        v.invalidate();
+                        break;
+                    }
+                }
+                return false;
+            }
+        });
     }
 }
